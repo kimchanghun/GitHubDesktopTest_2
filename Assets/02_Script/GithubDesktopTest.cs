e41897cd897392c68d6b5b045ab5aa9cc02039e6
kimchanghun <kchanghun@naver.com>
2026-01-14 14:31:33 +0900

    test1

---
+++ Assets/02_Script/GithubDesktopTest.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GithubDesktopTest : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
+
+// kim changhun unity project
