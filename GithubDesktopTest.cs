6e7b128fddbff7382a99eb635d515ee8645dd397
kimchanghun <kchanghun@naver.com>
2026-01-14 16:20:45 +0900

    PullTest

---
+++ GithubDesktopTest.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class GithubDesktopTest : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        print("a");
+        print("b");
+        print("one");
+        print("tow");
+    }
+
+    void Update()
+    {
+        
+    }
+}
+
+// kim changhun unity project
